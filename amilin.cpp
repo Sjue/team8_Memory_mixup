c59ed0e1f7762f2ec497a5c6508fb803caaacd88
antoniomilin <amilin@csub.edu>
2022-04-01 10:26:22 -0700

    Update amilin.cpp

--- amilin.cpp
+++ amilin.cpp
@@ -1,1 +1,12 @@
 //Antonio Milin
+#include <stdio.h>
+
+using namespace std;
+
+int main() {
+    //printing my name
+    cout << "Antonio Milin" << endl;
+    cout << "Class: CMPS 2250 - Software Engineering" << endl;
+    
+    return 0;
+}
