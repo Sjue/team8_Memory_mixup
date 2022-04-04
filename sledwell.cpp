38addf6a142df87065b7d8bee8bf227c582ba243
ledwell <sledwell@csub.edu>
2022-04-04 10:16:26 -0700

    Update sledwell.cpp

--- sledwell.cpp
+++ sledwell.cpp
@@ -2,9 +2,17 @@
 //Date: 02/24/2022
 //CMPS3350: Group Project - My Source Code
 
+bool sledwell(int n);
+
 int myMain()
 {
 //Adding some changes to see how github works
 
 return 0;
+}
+
+bool sledwell(int n)
+{
+    if (n%2) return 0;
+    return 1;
 }
