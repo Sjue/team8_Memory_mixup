20ee98fe7361c0ff6cd83b46256bfdc3deb0a252
antoniomilin <amilin@csub.edu>
2022-04-04 22:37:41 -0700

    Update bship.cpp

--- bship.cpp
+++ bship.cpp
@@ -414,12 +414,12 @@ void init_opengl(void)
 	w = bshipImage->width;
 	h = bshipImage->height;
 	glBindTexture(GL_TEXTURE_2D, bshipTexture);
-	//..glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MAG_FILTER,GL_NEAREST);
-	//..glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MIN_FILTER,GL_NEAREST);
+	glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MAG_FILTER,GL_NEAREST);
+	glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MIN_FILTER,GL_NEAREST);
 	//..glTexImage2D(GL_TEXTURE_2D, 0, 3, w, h, 0,
 								//..GL_RGB, GL_UNSIGNED_BYTE, bshipImage->data);
 	//-------------------------------------------------------------------------
-	//..glBindTexture(GL_TEXTURE_2D, 0);
+	glBindTexture(GL_TEXTURE_2D, 0);
 	//printf("tex: %i %i\n",Htexture,Vtexture);S
 }
 
