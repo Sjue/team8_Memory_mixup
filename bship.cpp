0247a5d7b26a6ff2e96ae87a239c57ced0a71ffc
antoniomilin <amilin@csub.edu>
2022-04-04 22:35:38 -0700

    Update bship.cpp

--- bship.cpp
+++ bship.cpp
@@ -414,13 +414,13 @@ void init_opengl(void)
 	w = bshipImage->width;
 	h = bshipImage->height;
 	glBindTexture(GL_TEXTURE_2D, bshipTexture);
-	glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MAG_FILTER,GL_NEAREST);
-	glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MIN_FILTER,GL_NEAREST);
-	glTexImage2D(GL_TEXTURE_2D, 0, 3, w, h, 0,
-								GL_RGB, GL_UNSIGNED_BYTE, bshipImage->data);
+	//..glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MAG_FILTER,GL_NEAREST);
+	//..glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MIN_FILTER,GL_NEAREST);
+	//..glTexImage2D(GL_TEXTURE_2D, 0, 3, w, h, 0,
+								//..GL_RGB, GL_UNSIGNED_BYTE, bshipImage->data);
 	//-------------------------------------------------------------------------
-	glBindTexture(GL_TEXTURE_2D, 0);
-	//printf("tex: %i %i\n",Htexture,Vtexture);
+	//..glBindTexture(GL_TEXTURE_2D, 0);
+	//printf("tex: %i %i\n",Htexture,Vtexture);S
 }
 
 void reset_grids(void)
