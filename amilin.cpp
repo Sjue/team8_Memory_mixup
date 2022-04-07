52924bf507203687801965acd961ca48dfef0644
AntonioMilin <98547778+antoniomilin@users.noreply.github.com>
2022-04-07 11:49:22 -0700

    Update amilin.cpp

--- amilin.cpp
+++ amilin.cpp
@@ -3,9 +3,9 @@
 //Midterm function
 int amilin (int n)
 {
-    int shift = 0;
-   if (n == XK_Shift_L || n == XK_Shift_R) {
-			shift=1;
-   }
-    return shift;
+	int flag = 0;
+	if (n == 1) {
+		flag = 1;
+	}
+	return flag;
 }
