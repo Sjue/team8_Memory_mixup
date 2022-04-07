74d07e693035fcc80e5417e67786b8dfd1444bdd
AntonioMilin <98547778+antoniomilin@users.noreply.github.com>
2022-04-07 11:12:13 -0700

    Update bship.cpp

--- bship.cpp
+++ bship.cpp
@@ -792,6 +792,7 @@ void get_grid_center(const int g, const int i, const int j, int cent[2])
 	//This determines the center of each grid.
 	switch(g) {
 		case 1:
+			//s0 = amilin(xres);
 			s0 = xres/4;
 			break;
 		case 2:
