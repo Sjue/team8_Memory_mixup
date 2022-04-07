d09fecab3d0d0c911a6b0025b16102635ac7fd2f
AntonioMilin <98547778+antoniomilin@users.noreply.github.com>
2022-04-07 10:35:23 -0700

    Update amilin.cpp

--- amilin.cpp
+++ amilin.cpp
@@ -2,13 +2,15 @@
 #include <iostream>
 
 using namespace std;
+
 //Midterm function
 int amilin (int n)
 {
-    //If fucntion returns 1 it means it is a positive number
     int count = 0;
-    if (n >= 0) {
-        cout << "The given argument is an positive number.\n";
+
+    if (n > 0) {
+        cout << "Number is a positive integer.\n";
+
         count = 1;
     }
     return count;
