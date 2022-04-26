37f940df3224d9cfd850fe133ea6aee70d199042
ledwell <sledwell@csub.edu>
2022-04-25 18:50:58 -0700

    Update sledwell.cpp

--- sledwell.cpp
+++ sledwell.cpp
@@ -2,9 +2,16 @@
 //Date: 02/24/2022
 //CMPS3350: Group Project - My Source Code
 #include "sledwell.h"
-#include <GL/glx.h>
-#include <GL/gl.h>
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <time.h>
+#include <math.h>
+#include <X11/Xlib.h>
+#include <X11/keysym.h>
+#include <GL/glx.h>
 
 bool sledwell(int n);
 
