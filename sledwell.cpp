c1c8ce49a86744d3b3116d3117dc183b9cb3479a
ledwell <sledwell@csub.edu>
2022-04-25 19:10:52 -0700

    Update sledwell.cpp

--- sledwell.cpp
+++ sledwell.cpp
@@ -11,7 +11,10 @@ class Gamestate gamestate;
 
 void Gamestate::showPage(int xres, int yres)
 {
-
+	Rect r;
+	r.bot = 50;
+	r.left = xres/2;
+	ggprint16(&r, 0, 0x0088aaff, "Press B to return to the Main Menu");
 }
 
 
