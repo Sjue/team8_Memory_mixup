5bdbec075a202d7f165a7efda9a5434e90df893a
sunolr <srasapatirath1@csub.edu>
2022-05-05 10:06:13 -0700

    changed cpp file

--- srasapatirat.cpp
+++ srasapatirat.cpp
@@ -1,10 +1,18 @@
 //sunol rasapatirath
+//associates ship numbers with an array of items in messy memory 
+//(10) banana, toy, crown, mallot, mush, wHat, egg, Lhat, Mhat, tie
 #include <stdio.h>
 
 void srasapatirat (int n) {
 
-        printf("number is %i", n);
-
+	
+	int items[10] = 0;
+	for (int i = 0; i < 10; i++) {
+		if (n == items[i]) {
+			int chosenItem = items[i];
+}
+}		
+	
    return;
 }
 
